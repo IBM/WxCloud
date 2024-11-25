cef5f2f9f62f22b21aba9229102b7b5800ecd651
srini <srinivasan.sabapathy@ibm.com>
2024-11-25 09:53:20 +0100

    Added Package Documentation

---
+++ pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://20847a026883:5555/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "25-11-2024 08:52:03 GMT";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"WxCloudAutomate","version":"1.0"};
+}
+});
